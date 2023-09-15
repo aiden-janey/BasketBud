6e52d5f41d6725df235317bcf28436a2bf8e2c1d
Aiden Janey <arjaney.professional@gmail.com>
2023-09-15 14:06:32 -0400

    Started Navigation Drawer on MainActivity

--- app/src/main/java/capstone/thriftytech/basketbud/MainActivity.kt
+++ app/src/main/java/capstone/thriftytech/basketbud/MainActivity.kt
@@ -3,9 +3,13 @@ package capstone.thriftytech.basketbud
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import capstone.thriftytech.basketbud.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -17,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     private val auth: FirebaseAuth = Firebase.auth
     var txtUser: TextView? = null
     var user: FirebaseUser? = null
+    lateinit var drawer: DrawerLayout
+    lateinit var actionBar: ActionBarDrawerToggle
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +32,30 @@ class MainActivity : AppCompatActivity() {
 
         txtUser = binding.userDetails
         user = auth.currentUser
+        drawer = binding.drawLayout
+        actionBar = ActionBarDrawerToggle(this, drawer, R.string.nav_open, R.string.nav_close)
+        navView = binding.navView
+
+        drawer.addDrawerListener(actionBar)
+        actionBar.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         if(user == null){
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
             finish()
-        }else{
+        }else {
             txtUser?.text = user?.email
         }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBar.onOptionsItemSelected(item)){
+            true
+        }else
+            super.onOptionsItemSelected(item)
     }
 
     fun logoutOfAcct(view: View) {
