a8af06040d05e23d09b572a02dc6cee7eab3745a
Khorn Eleptico <78042206+nrohK@users.noreply.github.com>
2023-09-25 21:21:39 -0400

    Added BottomNavView and Fragments for Project Features

--- app/src/main/java/capstone/thriftytech/basketbud/MainActivity.kt
+++ app/src/main/java/capstone/thriftytech/basketbud/MainActivity.kt
@@ -9,7 +9,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import capstone.thriftytech.basketbud.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -25,17 +28,31 @@ class MainActivity : AppCompatActivity() {
     lateinit var drawer: DrawerLayout
     lateinit var actionBar: ActionBarDrawerToggle
     lateinit var navView: NavigationView
+    lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        txtUser = binding.userDetails
+//        txtUser = binding.userDetails
         user = auth.currentUser
         drawer = binding.drawLayout
         actionBar = ActionBarDrawerToggle(this, drawer, R.string.nav_open, R.string.nav_close)
         navView = binding.navView
+        bottomNavigationView = binding.bottomNavigation
+
+        replaceFragment(HomeFragment())
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.basket -> replaceFragment(BasketFragment())
+                R.id.monthly_budget -> replaceFragment(MonthlyBudgetFragment())
+                R.id.notification -> replaceFragment(NotificationFragment())
+            }
+            true
+        }
 
         drawer.addDrawerListener(actionBar)
         actionBar.syncState()
@@ -49,6 +66,13 @@ class MainActivity : AppCompatActivity() {
         }else {
             txtUser?.text = user?.email
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.commit()
     }
 
 
