c54997d6197271abaa9f5bde8bb5b443030b437d
Aiden Janey <arjaney.professional@gmail.com>
2023-09-26 19:30:26 -0400

    Merged MainActivity files

--- app/src/main/java/capstone/thriftytech/basketbud/MainActivity.kt
+++ app/src/main/java/capstone/thriftytech/basketbud/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -13,6 +12,7 @@ import androidx.fragment.app.Fragment
 import capstone.thriftytech.basketbud.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -23,12 +23,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val auth: FirebaseAuth = Firebase.auth
-    var txtUser: TextView? = null
-    var user: FirebaseUser? = null
-    lateinit var drawer: DrawerLayout
-    lateinit var actionBar: ActionBarDrawerToggle
-    lateinit var navView: NavigationView
+    private lateinit var txtUser: TextView
+    private var user: FirebaseUser? = null
+    private lateinit var drawer: DrawerLayout
+    private lateinit var actionBar: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
     lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var scanBtn: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,12 +41,33 @@ class MainActivity : AppCompatActivity() {
         drawer = binding.drawLayout
         actionBar = ActionBarDrawerToggle(this, drawer, R.string.nav_open, R.string.nav_close)
         navView = binding.navView
+        scanBtn = binding.scanBtn
         bottomNavigationView = binding.bottomNavigation
+
+        drawer.addDrawerListener(actionBar)
+        actionBar.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Check if user is logged in
+        if (user == null) {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            txtUser?.text = user?.email
+        }
+
+        scanBtn.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         replaceFragment(HomeFragment())
 
         bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.home -> replaceFragment(HomeFragment())
                 R.id.basket -> replaceFragment(BasketFragment())
                 R.id.monthly_budget -> replaceFragment(MonthlyBudgetFragment())
@@ -54,18 +76,6 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        drawer.addDrawerListener(actionBar)
-        actionBar.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        if(user == null){
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish()
-        }else {
-            txtUser?.text = user?.email
-        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -75,12 +85,8 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (actionBar.onOptionsItemSelected(item)){
-            true
-        }else if(item.itemId == R.id.nav_logout){
-            logoutOfAcct()
             true
         } else
             super.onOptionsItemSelected(item)
@@ -88,13 +94,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun logoutOfAcct() {
         FirebaseAuth.getInstance().signOut()
-        val intent = Intent(this, Login::class.java)
-        startActivity(intent)
-        finish()
-        Toast.makeText(this, "Successful Logout", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun openCamera(){
         val intent = Intent(this, Login::class.java)
         startActivity(intent)
         finish()
