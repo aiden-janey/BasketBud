4d840ab64429f21ff73e1077de49b8406215072e
Khorn Eleptico <78042206+nrohK@users.noreply.github.com>
2023-10-25 19:26:51 -0400

    App Bar, Expense Class, ImageView, and confirm before upload image with scan receipt feature,

---
+++ app/src/main/java/capstone/thriftytech/basketbud/HomeFragment.kt
@@ -0,0 +1,44 @@
+package capstone.thriftytech.basketbud
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import capstone.thriftytech.basketbud.databinding.ActivityCameraBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: ActivityCameraBinding
+    private val user = Firebase.auth.currentUser
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showUserName()
+    }
+
+    private fun showUserName() {
+        user?.reload()
+        val userNameTV: TextView = requireView().findViewById(R.id.userNameTV)
+        val email = user!!.email
+        val userName = user.displayName
+
+        val name = if (userName == null || userName == ""){
+            val splitValue = email?.split("@")
+            splitValue?.get(0).toString()
+        }else{
+            userName
+        }
+
+        userNameTV.text = "Hello, ${name}"
+    }
+}
---
+++ app/src/main/java/capstone/thriftytech/basketbud/data/Expense.kt
@@ -0,0 +1,16 @@
+package capstone.thriftytech.basketbud.data
+
+class Expense() {
+    var imageUrl: String? = null
+    var purchaseDate: String? = null
+    var purchaseTotal: Double? = null
+    var store: String? = null
+    var userId: String? = null
+    init {
+        imageUrl = "IMAGE_URL"
+        purchaseDate = "99/99/9999"
+        purchaseTotal = 99999.99
+        store = "STORE"
+        userId = "USER_ID"
+    }
+}
