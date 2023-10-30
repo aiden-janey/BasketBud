dabab109a1695ef757f01c65e2e8577bd986272b
wongtho <78035657+wongtho@users.noreply.github.com>
2023-10-29 20:41:54 -0400

    Updated work bank

--- app/src/main/java/capstone/thriftytech/basketbud/data/BasketListBank.kt
+++ app/src/main/java/capstone/thriftytech/basketbud/data/BasketListBank.kt
@@ -5,5 +5,18 @@ class BasketListBank {
         "Potatoes", "Rice", "Pasta", "Canned Tomatoes", "Cereal", "Orange Juice", "Coffee", "Tea",
         "Sugar", "Flour", "Salt", "Pepper", "Cheese", "Onions", "Carrots", "Lettuce", "Tomatoes",
         "Cucumbers", "Toilet Paper", "Soap", "Shampoo", "Toothpaste", "Dish Soap", "Laundry Detergent",
-        "Trash Bags", "Aluminum Foil", "Plastic Wrap")
+        "Trash Bags", "Aluminum Foil", "Plastic Wrap", "Oranges", "Berries", "Grapes", "Lemons",
+        "Avocados", "Broccoli", "Bell peppers", "Yogurt", "Chicken breasts/thighs", "Ground beef",
+        "Fish fillets", "Bacon", "Sausages", "Bagels", "Rolls", "Croissants", "Muffins", "Pantry Staples:",
+        "Canned beans", "Canned tomatoes", "Oil", "Frozen vegetables", "Frozen fruits", "Frozen pizza",
+        "Ice cream", "Frozen meals", "Chips", "Crackers", "Nuts", "Popcorn", "Granola bars", "Water",
+        "Juice", "Soft drinks", "Ketchup", "Mustard", "Mayonnaise", "Soy sauce", "Salad dressing",
+        "Pasta sauce", "Cooking oil", "Oatmeal", "Pancake mix", "Maple syrup", "Deli meats",
+        "Freshly prepared salads", "Rotisserie chicken", "Hummus", "Pre-made sandwiches",
+        "Black pepper", "Garlic powder", "Cinnamon", "Paprika", "Basil", "Oregano", "Cumin",
+        "Baking powder", "Baking soda", "Vanilla extract", "Chocolate chips", "Yeast", "Tuna",
+        "Soup", "Corn", "Peas", "Canned fruit", "Diapers", "Baby formula", "Baby food", "Baby wipes",
+        "Paper towels", "Toilet paper", "Laundry detergent", "Dish soap", "Trash bags", "Conditioner",
+        "Deodorant", "Dog food", "Cat food", "Pet treats", "Litter", "Sushi rice", "Curry paste",
+        "Tortillas", "Salsa", "Coconut milk")
 }
