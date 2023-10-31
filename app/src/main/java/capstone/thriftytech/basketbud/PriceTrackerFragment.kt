ca145bcc6b319602c2b6eb36e89247d299d0d596
Manvir Sohi <sohimanv@sheridancollege.ca>
2023-10-31 00:33:09 -0400

    Price Tracking feature mostly finished (Search function still not working as expected)

--- app/src/main/java/capstone/thriftytech/basketbud/PriceTrackerFragment.kt
+++ app/src/main/java/capstone/thriftytech/basketbud/PriceTrackerFragment.kt
@@ -1,18 +1,30 @@
 package capstone.thriftytech.basketbud
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import capstone.thriftytech.basketbud.data.Product
+import capstone.thriftytech.basketbud.data.Store
 import capstone.thriftytech.basketbud.databinding.ActivityCameraBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.ArrayList
 
 class PriceTrackerFragment : Fragment() {
     private lateinit var binding: ActivityCameraBinding
     private val user = Firebase.auth.currentUser
+    private val originalProductList = ArrayList<Product>()
+    private val filteredProductList = ArrayList<Product>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +38,22 @@ class PriceTrackerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         showUserName()
+        fetchUserProducts()
+
+        val searchView: SearchView = requireView().findViewById(R.id.searchView)
+
+        searchView.queryHint = "Search Products"
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterProducts(newText ?: "")
+                return true
+            }
+        })
     }
 
     private fun showUserName() {
@@ -43,4 +71,85 @@ class PriceTrackerFragment : Fragment() {
 
         userNameTV.text = "Hello, ${name}"
     }
+
+    private fun fetchUserProducts() {
+        val currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
+
+        if (currentUserUid != null) {
+            val firestore = FirebaseFirestore.getInstance()
+
+            firestore.collection("products")
+                .whereEqualTo("user_id", currentUserUid)
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    val productsList = ArrayList<Product>()
+
+                    for (document in querySnapshot.documents) {
+                        val product = document.toObject(Product::class.java)
+                        val storeId = product?.store_id
+
+                        if (storeId != null) {
+                            firestore.collection("stores").document(storeId).get()
+                                .addOnSuccessListener { storeDocument ->
+                                    val store = storeDocument.toObject(Store::class.java)
+
+                                    val productItem = Product(
+                                        product.buy_date ?: "",
+                                        product.prod_name ?: "",
+                                        product.prod_price ?: "",
+                                        store?.store_name ?: ""
+                                    )
+
+                                    productsList.add(productItem)
+
+                                    val recyclerView: RecyclerView =
+                                        requireView().findViewById(R.id.recyclerView)
+                                    val layoutManager = LinearLayoutManager(context)
+                                    val adapter = context?.let { ProductAdapter(it, productsList) }
+                                    recyclerView.layoutManager = layoutManager
+                                    recyclerView.adapter = adapter
+
+                                    adapter?.notifyDataSetChanged()
+                                }
+                                .addOnFailureListener { exception ->
+                                    val errorMessage = "Failed to retrieve store data: ${exception.message}"
+                                    Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+
+                                    Log.e("FirestoreError", "Failed to retrieve store data: ${exception.message}")
+                                }
+                        }
+                    }
+
+                    originalProductList.clear()
+                    originalProductList.addAll(productsList)
+                }
+                .addOnFailureListener { exception ->
+                    val errorMessage = "Failed to retrieve product data: ${exception.message}"
+                    Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
+
+                    Log.e("FirestoreError", errorMessage)
+                }
+        }
+    }
+
+    private fun filterProducts(query: String) {
+        val recyclerView: RecyclerView = requireView().findViewById(R.id.recyclerView)
+        val layoutManager = LinearLayoutManager(context)
+
+        if (query.isBlank()) {
+            val adapter = context?.let { ProductAdapter(it, originalProductList) }
+            recyclerView.layoutManager = layoutManager
+            recyclerView.adapter = adapter
+        } else {
+            val filteredList = originalProductList.filter { product ->
+                product.prod_name?.contains(query, ignoreCase = true) == true
+            }
+            val adapter = context?.let { ProductAdapter(it, filteredProductList) }
+            recyclerView.layoutManager = layoutManager
+            recyclerView.adapter = adapter
+        }
+
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
 }
---
+++ app/src/main/java/capstone/thriftytech/basketbud/ProductAdapter.kt
@@ -0,0 +1,43 @@
+package capstone.thriftytech.basketbud
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import capstone.thriftytech.basketbud.data.Product
+
+class ProductAdapter(var context: Context, var productsList: ArrayList<Product>) :
+    RecyclerView.Adapter<ProductAdapter.MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val v = LayoutInflater.from(context).inflate(R.layout.product, parent, false)
+        return MyViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val (buy_date, prod_name, prod_price, store_id) = productsList[position]
+        holder.buyDate.text = buy_date
+        holder.productName.text = prod_name
+        holder.productPrice.text = prod_price
+        holder.storeName.text = store_id
+    }
+
+    override fun getItemCount(): Int {
+        return productsList.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var buyDate: TextView
+        var productName: TextView
+        var productPrice: TextView
+        var storeName: TextView
+
+        init {
+            buyDate = itemView.findViewById(R.id.buyDate)
+            productName = itemView.findViewById(R.id.productName)
+            productPrice = itemView.findViewById(R.id.productPrice)
+            storeName = itemView.findViewById(R.id.storeName)
+        }
+    }
+}
